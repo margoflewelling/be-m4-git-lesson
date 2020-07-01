9cae0abce4f8bd8e3636293a0a07a92ad810cffc
Margo Flewelling <flewelling.margo@gmail.com>
2020-07-01 10:01:38 -0600

    Remove total paid method, edit header, and add colon

--- app/models/order.rb
+++ app/models/order.rb
@@ -25,10 +25,10 @@ class Order < ApplicationRecord
   def self.total_ordered
     where(status: 0).count
   end
-
-  def self.total_paid
-    where(status: 1).count
-  end
+  #
+  # def self.total_paid
+  #   where(status: 1).count
+  # end
 
   def self.total_cancelled
     where(status: 2).count
